9eed26abb4f35bd005ba629bca2823d596f31a6d
Walter Soprani Junior <waltersopranijr@gmail.com>
2025-04-05 08:00:00 -0300

    Encapsulamento simples com chamada do objeto Funcionário no pacote empresa.dados.

---
+++ PackageEmpresa/src/empresa/dados/Funcionario.java
@@ -0,0 +1,13 @@
+package empresa.dados;
+
+public class Funcionario {
+	String nome;
+	double salario;
+	
+	public Funcionario(String nomeInit, double salarioInit) {
+		nome = nomeInit;
+		salario = salarioInit;
+		System.out.println("Nome: " + nome);
+		System.out.println("Salário: " + salario);
+	}
+}
