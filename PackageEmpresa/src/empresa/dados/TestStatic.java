051f357ef9119c94d33f17ecc8acbc6688835d99
Walter Soprani Junior <waltersopranijr@gmail.com>
2025-04-10 08:20:00 -0300

    METODO ESTATICO: remoção do static de exibir valores, sendo necessario criar um objeto dentro do main

--- PackageEmpresa/src/empresa/dados/TestStatic.java
+++ PackageEmpresa/src/empresa/dados/TestStatic.java
@@ -4,13 +4,14 @@ package empresa.dados;
 
 public class TestStatic {
 	
-	public static void exibirValor(int a) {
+	public void exibirValor(int a) {
 		System.out.println("O valor do inteiro é: " + a);
 	}
 	
 	public static void main(String[] args) {
+		TestStatic ts = new TestStatic();
 		
-		exibirValor(10);
+		ts.exibirValor(10);
 		
 	}
 
