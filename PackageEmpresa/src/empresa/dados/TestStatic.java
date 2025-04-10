256cf3158d2690c9951045660ed69a4edbc676b8
Walter Soprani Junior <waltersopranijr@gmail.com>
2025-04-10 08:00:00 -0300

    METODO ESTATICO: criação de uma classe para teste e entendimento do static

---
+++ PackageEmpresa/src/empresa/dados/TestStatic.java
@@ -0,0 +1,17 @@
+package empresa.dados;
+
+//classe para testar o static
+
+public class TestStatic {
+	
+	public static void exibirValor(int a) {
+		System.out.println("O valor do inteiro é: " + a);
+	}
+	
+	public static void main(String[] args) {
+		
+		TestStatic.exibirValor(10);
+		
+	}
+
+}
