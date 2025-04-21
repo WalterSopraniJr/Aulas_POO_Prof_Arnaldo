ba81816174917a5168faf874624ea6666bd8e7b6
Walter Soprani Junior <waltersopranijr@gmail.com>
2025-04-21 09:00:00 -0300

    EXERCICIO acrescimo do laco tradicional na impressao dos valores

--- ExercicioSalaDeAula/src/PreenchendoEImprimindoArrayPar.java
+++ ExercicioSalaDeAula/src/PreenchendoEImprimindoArrayPar.java
@@ -9,11 +9,16 @@ public class PreenchendoEImprimindoArrayPar {
 			 inteiros[i] = 2 * (i + 1);
 		}
 
-		
+		System.out.print("For-each: ");
 		for(int i : inteiros) {
-			
 			System.out.print(i + " ");
-			
+		}
+		
+		System.out.println("\n");
+		
+		System.out.print("For tradicional: ");		
+		for(int i = 0; i < tamanho; i++) {
+			System.out.print(inteiros[i] + " ");
 		}
 	}
 
