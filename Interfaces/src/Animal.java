5b325764ecafae03c8357ad338d923aff8cac129
Walter Soprani Junior <waltersopranijr@gmail.com>
2025-08-06 18:31:21 -0300

    Interfaces podem estender outras interfaces, assim, sendo obrigatório colocar os demais métodos nas demais classes

--- Interfaces/src/Animal.java
+++ Interfaces/src/Animal.java
@@ -1,17 +1,28 @@
+interface SerVivo{
+	void respirar();
+}
 
-interface ReinoAnimal {
+interface ReinoAnimal extends SerVivo {
 	void fazerSom(); //Método Abstrato
 }
 
 
 class Cachorro implements ReinoAnimal{
 
+	public void respirar() {
+		System.out.println("O cachorro respira."); //Definição do método
+	}
+	
 	public void fazerSom() {
 		System.out.println("O cachorro late."); //Definição do método
 	}
 }
 
 class Gato implements ReinoAnimal{
+
+	public void respirar() {
+		System.out.println("O gato respira."); //Definição do método
+	}
 	
 	public void fazerSom() {
 		System.out.println("O gato mia."); //Definição do método
@@ -23,8 +34,10 @@ public class Animal{
 		
 		Cachorro cachorro = new Cachorro();
 		cachorro.fazerSom();
+		cachorro.respirar();
 		
 		Gato gato = new Gato();
 		gato.fazerSom();
+		gato.respirar();
 	}
 }
